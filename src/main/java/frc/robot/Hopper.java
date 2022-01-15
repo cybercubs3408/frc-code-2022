1420c138e1d94970607efa892825f2ff4e80beab
Sprocket <robotics@loyolahs.edu>
2022-01-15 12:11:37 -0800

    1-15-22 D

--- src/main/java/frc/robot/Hopper.java
+++ src/main/java/frc/robot/Hopper.java
@@ -1,5 +1,64 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 public class Hopper {
     
+    TalonSRX topHopperMotor, bottomHopperMotor;
+
+    /**
+     * Constructor class defining hopper motors
+     * @param topHopperID ID of the top hopper motor
+     * @param bottomHopperID ID of the bottom hopper motor
+     */
+    public Hopper (int topHopperID, int bottomHopperID) {
+
+        topHopperMotor = new TalonSRX(topHopperID);
+        bottomHopperMotor = new TalonSRX(bottomHopperID);
+
+    }
+
+    /**
+     * Method to set the inversion statuses of the hopper motors
+     * @param topHopperInverted Boolean for whether or not the top hopper motor is inverted
+     * @param bottomHopperInverted Boolean for whether or not the bottom hopper motor is inverted
+     */
+    public void setInversion (boolean topHopperInverted, boolean bottomHopperInverted) {
+        
+        topHopperMotor.setInverted(topHopperInverted);
+        bottomHopperMotor.setInverted(bottomHopperInverted);
+
+    }
+
+    /**
+     * Method to control the intake of the hopper
+     * @param power Undefined power level for the intake of the hopper motors
+     */
+    public void hopperIn (double power) {
+
+        topHopperMotor.set(ControlMode.PercentOutput, Math.abs(power));
+        bottomHopperMotor.set(ControlMode.PercentOutput, Math.abs(power));
+
+    }
+    /**
+     * Method to control the outtake of the hopper
+     * @param power2 Undefined power level for the outtake of the hopper motors
+     */
+    public void hopperOut (double power2) {
+
+        topHopperMotor.set(ControlMode.PercentOutput, -Math.abs(power2));
+        bottomHopperMotor.set(ControlMode.PercentOutput, -Math.abs(power2));
+
+    }
+
+    /**
+     * Method to stop all hopper motors
+     */
+    public void stop () {
+
+        topHopperMotor.set(ControlMode.PercentOutput, 0);
+        bottomHopperMotor.set(ControlMode.PercentOutput, 0);
+
+    }
 }
--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -1,5 +1,58 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 public class Intake {
     
+    TalonSRX intakeMotor;
+
+    /**
+     * Constructor class defining the motor for the robots intake system
+     * @param intakeMotorID ID of the intake motor
+     */
+    public Intake (int intakeMotorID) {
+
+        intakeMotor = new TalonSRX(intakeMotorID);
+
+    }
+
+    /**
+     * Method to set the inversion status of the intake motor
+     * @param intakeMotorInverted Boolean for the inversion status of the intake motor
+     */
+    public void setInversion (boolean intakeMotorInverted) {
+
+        intakeMotor.setInverted(intakeMotorInverted);
+
+    }
+
+    /**
+     * Method to run the intake 
+     * @param power Unspecified power to run the intake motor
+     */
+    public void intakeIn (double power) {
+
+        intakeMotor.set(ControlMode.PercentOutput, Math.abs(power));
+
+    }
+
+    /**
+     * Method to run the intake in reverse
+     * @param power2 Unspecified negative power for the intake motor
+     */
+    public void intakeOut (double power2) {
+
+        intakeMotor.set(ControlMode.PercentOutput, -Math.abs(power2));
+
+    }
+
+    /**
+     * Method to stop the intake motor
+     */
+    public void stop () {
+
+        intakeMotor.set(ControlMode.PercentOutput, 0);
+
+    }
 }
